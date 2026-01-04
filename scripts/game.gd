833ba5a1952eb3e5fb7e1cb945cac4c7c0b0c19a
Almonds2 <williammusanto@gmail.com>
2026-01-05 00:55:21 +0700

    add placement mode (not working)

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,28 +1,46 @@
 extends Node2D
 
+class_name Game
+
 @onready var tilemap_VAR = $TileMapLayer
 @onready var ghost_block_VAR = $ghost_block
+
+var is_placement_mode: bool
+var is_inventory_mode: bool
 
 var convenyor_tileset_VAR := 1
 var convenyor_tileloc_VAR := Vector2i(0, 0)
 
 func _ready() -> void:
-	pass
+	add_to_group("game")
+	is_placement_mode = false
+	is_inventory_mode = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("inventory") and is_inventory_mode == false:
+		is_inventory_mode = true
+		is_placement_mode = false
+	elif Input.is_action_just_pressed("inventory") and is_inventory_mode == true:
+		is_inventory_mode = false
+	
 	var mouse_pos_VAR = get_global_mouse_position()
 	var local = tilemap_VAR.to_local(mouse_pos_VAR)
 	var cell = tilemap_VAR.local_to_map(local)
 	var snap_local = tilemap_VAR.map_to_local(cell)
 	
-	if Input.is_action_pressed("MOUSE_LEFT_CLICK"):
+	if Input.is_action_pressed("MOUSE_LEFT_CLICK") and is_placement_mode == true:
 		var local_pos = tilemap_VAR.to_local(ghost_block_VAR.global_position)
 		var cell_tilemap = tilemap_VAR.local_to_map(local_pos)
-
+				
 		tilemap_VAR.set_cell(cell_tilemap, 1, Vector2i(0, 0), TileSetAtlasSource.TRANSFORM_FLIP_H)
-		print("left")
+	
+func is_place_func(bool_VAR: bool):
+	is_placement_mode = bool_VAR
+
+func is_inv_func(bool_VAR: bool):
+	is_inventory_mode = bool_VAR
 	
 	
 	
--- scripts/inv_ui_slot.gd
+++ scripts/inv_ui_slot.gd
@@ -1,10 +1,38 @@
 extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+@onready var button = $Button
+var item_name_VAR: String
 
-func update(item: InvItem):
+@onready var Game_script: Game = get_tree().current_scene
+
+var is_placement_mode: bool
+
+func _ready() -> void:
+	pass
+
+func update(item: InvItem):	
 	if !item:
 		item_visual.visible = false
 	else:
 		item_visual.visible = true
 		item_visual.texture = item.texture
+		item_name_VAR = item.name
+	
+func _process(_delta: float) -> void:
+	is_placement_mode = Game_script.is_placement_mode
+	
+	print(is_placement_mode)
+	
+	if button.button_pressed and Input.is_action_just_pressed("MOUSE_LEFT_CLICK"):
+		if is_placement_mode:
+			Game_script.is_place_func(false)
+		elif is_placement_mode == false:
+			Game_script.is_place_func(true)
+			if item_name_VAR == "convenyor":
+				print("convenyor")
+	
+			
+
+		
+		
